f2b28fa11fe03125909e71220bc639f7b4e3b56c
gompali <yannick.gger@gmail.com>
2024-05-25 09:35:28 +0200

    Reconfig

---
+++ backend/src/Presentation/Http/Api/TimeAction.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Presentation\Http\Api;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(path: '/time', name: 'time', methods: 'GET')]
+class TimeAction
+{
+    public function __invoke(Request $request): JsonResponse
+    {
+        $time = new \DateTimeImmutable('now');
+
+        return new JsonResponse($time, Response::HTTP_OK);
+    }
+}
